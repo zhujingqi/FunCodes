38c0f938e5ab72e14e0c7c6efa5859f008ec66eb
zhujingqi <76515195+zhujingqi@users.noreply.github.com>
2024-10-16 06:33:00 +0800

    Update and rename 恶意窗口.cpp to unclosable_window.cpp

--- unclosable_window.cpp
+++ unclosable_window.cpp
@@ -1,30 +1,25 @@
-#include <windows.h>
+/*This program is by Jacky Zhu.
+He got some help from AI.*/
 
+#include <windows.h>
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
     {
         case WM_CLOSE:
-            // 当用户尝试关闭窗口时，我们忽略这个消息
             return 0;
-
         case WM_DESTROY:
-            // 当窗口被销毁时，我们重新创建它
             PostQuitMessage(0);
             return 0;
     }
-
     return DefWindowProc(hwnd, msg, wParam, lParam);
 }
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	FreeConsole();
+    FreeConsole();
     WNDCLASSEX wcex;
     HWND hwnd;
     MSG msg;
-
-    // 注册窗口类
     wcex.cbSize = sizeof(WNDCLASSEX);
     wcex.style = CS_HREDRAW | CS_VREDRAW;
     wcex.lpfnWndProc = WindowProcedure;
@@ -37,31 +32,23 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wcex.lpszMenuName = NULL;
     wcex.lpszClassName = "UnclosableWindowClass";
     wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-
     if (!RegisterClassEx(&wcex))
     {
         MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
         return 0;
     }
-
-    // 创建窗口
     hwnd = CreateWindow("UnclosableWindowClass", "Unclosable Window", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 500, 500, NULL, NULL, hInstance, NULL);
-
     if (!hwnd)
     {
         MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
         return 0;
     }
-
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
-
-    // 消息循环
     while (GetMessage(&msg, NULL, 0, 0))
     {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-
     return msg.wParam;
 }
