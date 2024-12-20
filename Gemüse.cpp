9463d3d91c9e4958256193fc285f582132b88ebb
zhujingqi <76515195+zhujingqi@users.noreply.github.com>
2024-12-20 21:47:15 +0800

    Add files via upload

---
+++ Gemüse.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+	FreeConsole();
+    for(;;)
+    {
+    	MessageBox(NULL,"Gem¨¹se!","Gem¨¹se!",MB_OK);
+    	Sleep(10000);
+	}
+    return 0;
+}
